2a0e2985209085557623d521b09f97bfab76baf4
seekincs <seekincs@gmail.com>
2019-12-02 21:25:46 +0800

    init goods-info.

---
+++ src/main/java/com/tardybird/goodsinfo/GoodsInfoApplication.java
@@ -0,0 +1,13 @@
+package com.tardybird.goodsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tardybird/goodsinfo/GoodsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tardybird.goodsinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
