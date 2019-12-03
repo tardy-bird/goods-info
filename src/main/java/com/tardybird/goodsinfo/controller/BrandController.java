be525f922ebd1a122bdad861b1bacab4ae7c3973
seekincs <seekincs@gmail.com>
2019-12-03 23:58:41 +0800

    update BrandController APIs.

--- src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
+++ src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
@@ -1,8 +1,6 @@
 package com.tardybird.goodsinfo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author nick
@@ -28,6 +26,39 @@ public class BrandController {
     @GetMapping("/brands/{id}")
     public Object getBrandDetails(@PathVariable("id") Long id) {
         return null;
+    }
+
+    /*
+     * ========= following are admin apis ==============
+     */
+
+    /**
+     * 根据条件搜索品牌
+     */
+    @GetMapping("/admins/brands")
+    public Object getBrandsByCondition() {
+        return null;
+    }
+
+    /**
+     * 创建一个品牌
+     */
+    @PostMapping("/brands")
+    public void addBrand() {
+    }
+
+    /**
+     * 修改单个品牌的信息
+     */
+    @PutMapping("/brands/{id}")
+    public void updateBrand(@PathVariable("id") Long id) {
+    }
+
+    /**
+     * 删除一个品牌
+     */
+    @DeleteMapping("/brands/{id}")
+    public void deleteBrand(@PathVariable("id") Long id) {
     }
 
 }
