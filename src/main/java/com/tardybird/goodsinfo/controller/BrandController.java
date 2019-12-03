6b5ab94577b03867702df95007ebcdf9ce5e0b63
seekincs <seekincs@gmail.com>
2019-12-03 13:28:17 +0800

    fill BrandController APIs.

---
+++ src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
@@ -0,0 +1,31 @@
+package com.tardybird.goodsinfo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author nick
+ */
+public class BrandController {
+
+    /*
+     * ========= following are wx apis ==============
+     */
+
+    /**
+     * 查看所有品牌
+     */
+    @GetMapping("/brands")
+    public Object getAllBrands() {
+        return null;
+    }
+
+    /**
+     * 查看品牌详情
+     */
+    @GetMapping("/brands/{id}")
+    public Object getBrandDetails(@PathVariable("id") Long id) {
+        return null;
+    }
+
+}
