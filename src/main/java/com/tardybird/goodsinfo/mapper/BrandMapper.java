d0c313a68827b89a2a83ed68fce89a60caa063f0
nan <2324737911@qq.com>
2019-12-04 00:46:06 +0800

    阿里规约

--- src/main/java/com/tardybird/goodsinfo/mapper/BrandMapper.java
+++ src/main/java/com/tardybird/goodsinfo/mapper/BrandMapper.java
@@ -1,12 +1,14 @@
 package com.tardybird.goodsinfo.mapper;
 
 import com.tardybird.goodsinfo.domain.Brand;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Mapper
+
+/**
+ * @author LENOVO
+ */
 @Repository
 public interface BrandMapper {
 
