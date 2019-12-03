d605bf2afa00c7993706c30985e89225f0b7c61e
seekincs <seekincs@gmail.com>
2019-12-03 13:31:57 +0800

    fix fault: without @RestController.

--- src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
+++ src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
@@ -2,10 +2,12 @@ package com.tardybird.goodsinfo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author nick
  */
+@RestController
 public class BrandController {
 
     /*
--- src/main/java/com/tardybird/goodsinfo/controller/ProductController.java
+++ src/main/java/com/tardybird/goodsinfo/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.tardybird.goodsinfo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author nick
  */
+@RestController
 public class ProductController {
     /*
      * nothing yet
