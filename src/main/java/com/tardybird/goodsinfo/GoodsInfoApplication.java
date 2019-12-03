1890bbfc185c32012a8c8fa68fa4c9f780188f6d
nan <2324737911@qq.com>
2019-12-04 00:36:17 +0800

    add Brand.xml

--- src/main/java/com/tardybird/goodsinfo/GoodsInfoApplication.java
+++ src/main/java/com/tardybird/goodsinfo/GoodsInfoApplication.java
@@ -1,5 +1,7 @@
 package com.tardybird.goodsinfo;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author nick
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.tardybird.goodsinfo.mapper")
 public class GoodsInfoApplication {
 
     public static void main(String[] args) {
