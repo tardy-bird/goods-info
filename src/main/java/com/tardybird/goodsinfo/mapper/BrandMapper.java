8a529689104de8153eb3847cb6ea3598271018f3
nan <2324737911@qq.com>
2019-12-04 21:28:00 +0800

    add brandmapper

--- src/main/java/com/tardybird/goodsinfo/mapper/BrandMapper.java
+++ src/main/java/com/tardybird/goodsinfo/mapper/BrandMapper.java
@@ -1,6 +1,8 @@
 package com.tardybird.goodsinfo.mapper;
 
 import com.tardybird.goodsinfo.domain.Brand;
+import io.swagger.models.auth.In;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,7 @@ import java.util.List;
 public interface BrandMapper {
 
     List<Brand> gerAllBrands();
-
+    Brand getBrandsById(@Param("id") Integer id);
+    Brand addBrand(@Param("brand") Brand brand);
+    boolean deleteBrand(@Param("id")Integer id);
 }
