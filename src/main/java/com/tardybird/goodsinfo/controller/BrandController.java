c2961f513b276f9c5aec4ada6368633ca9e136b9
zjssss <935374257@qq.com>
2019-12-05 20:49:35 +0800

    commit Goods.xml

--- src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
+++ src/main/java/com/tardybird/goodsinfo/controller/BrandController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @author nick
@@ -31,10 +32,10 @@ public class BrandController {
      * 查看所有品牌
      */
     @GetMapping("/brands")
-    public Object getAllBrands(@RequestParam(defaultValue = "1") Integer page,
-                               @RequestParam(defaultValue = "10") Integer limit,
-                               @Sort @RequestParam(defaultValue = "add_time") String sort,
-                               @Order @RequestParam(defaultValue = "desc") String order) {
+    public List<Brand> getAllBrands(@RequestParam(defaultValue = "1") Integer page,
+                                    @RequestParam(defaultValue = "10") Integer limit,
+                                    @Sort @RequestParam(defaultValue = "add_time") String sort,
+                                    @Order @RequestParam(defaultValue = "desc") String order) {
         return brandService.getAllBrands();
     }
 
@@ -42,7 +43,7 @@ public class BrandController {
      * 查看品牌详情
      */
     @GetMapping("/brands/{id}")
-    public Object getBrandDetails(@NotNull @PathVariable("id") Integer id) {
+    public Brand getBrandDetails(@NotNull @PathVariable("id") Integer id) {
         return brandService.getBrandsById(id);
     }
 
