89e3e1adc568d57497d96dd501275b6f8a97b1ce
seekincs <seekincs@gmail.com>
2019-12-08 22:49:55 +0800

    pass all tests about Brand.

---
+++ src/test/java/com/tardybird/goodsinfo/controller/BrandControllerTest.java
@@ -0,0 +1,59 @@
+package com.tardybird.goodsinfo.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BrandControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllBrands() {
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/brands"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andDo(MockMvcResultHandlers.print());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getBrandDetails() {
+    }
+
+    @Test
+    void getBrandsByCondition() {
+    }
+
+    @Test
+    void addBrand() {
+    }
+
+    @Test
+    void updateBrand() {
+    }
+
+    @Test
+    void deleteBrand() {
+    }
+}
