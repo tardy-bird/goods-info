f66f788ea256a8e843edc2ed599800676b23ca13
seekincs <seekincs@gmail.com>
2019-12-09 22:18:25 +0800

    remove some methods.

--- src/main/java/com/tardybird/goodsinfo/dao/GoodsDao.java
+++ src/main/java/com/tardybird/goodsinfo/dao/GoodsDao.java
@@ -5,7 +5,6 @@ import com.tardybird.goodsinfo.domain.Goods;
 import com.tardybird.goodsinfo.domain.Product;
 import com.tardybird.goodsinfo.mapper.GoodsMapper;
 import com.tardybird.goodsinfo.mapper.ProductMapper;
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,7 +15,6 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author DIX
  * @version 1.0
- * @description
  * @date 2019/12/6 19:39
  */
 
@@ -24,71 +22,68 @@ import java.util.concurrent.TimeUnit;
 public class GoodsDao {
 
     @Autowired
-    private GoodsMapper goodsMapper;
+    GoodsMapper goodsMapper;
 
     @Autowired
-    private ProductMapper productMapper;
+    ProductMapper productMapper;
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    RedisTemplate redisTemplate;
 
     @Autowired
-    private RedisConfig redisConfig;
+    RedisConfig redisConfig;
 
     /**
      * 通过id获取product
-     * @param id
-     * @return
+     *
+     * @param id x
+     * @return x
      */
-    public Product getProductById(Integer id)
-    {
-        String key="P_"+id;
-        Product product=(Product)redisTemplate.opsForValue().get(key);
-        if(product==null)
-        {
-            product=productMapper.getProductById(id);
-            redisTemplate.opsForValue().set(key,product,redisConfig.getRedisExpireTime(), TimeUnit.MINUTES);
+    public Product getProductById(Integer id) {
+        String key = "P_" + id;
+        Product product = (Product) redisTemplate.opsForValue().get(key);
+        if (product == null) {
+            product = productMapper.getProductById(id);
+            redisTemplate.opsForValue().set(key, product, redisConfig.getRedisExpireTime(), TimeUnit.MINUTES);
         }
         return product;
     }
 
     /**
      * 返回一个goods的所有product
-     * @param id
-     * @return
+     *
+     * @param id x
+     * @return x
      */
-    public List<Product> getProductByGoodsId(Integer id)
-    {
+    public List<Product> getProductByGoodsId(Integer id) {
         return productMapper.getProductByGoodsId(id);
     }
 
     /**
      * 通过id获得商品（包括他的product）
-     * @param id
-     * @return
+     *
+     * @param id x
+     * @return x
      */
-    public Goods getGoodsById(Integer id)
-    {
-        String key="G_"+id;
-        Goods goods =(Goods) redisTemplate.opsForValue().get(key);
-        if(goods==null)
-        {
-            System.out.println(key);
-            goods=goodsMapper.getGoodsById(id);
-            List<Product> goodsProduct=this.getProductByGoodsId(id);
-//            for(Product item:goodsProduct){
-//                System.out.println("1"+item.toString());}
+    public Goods getGoodsById(Integer id) {
+        String key = "G_" + id;
+        Goods goods = (Goods) redisTemplate.opsForValue().get(key);
+        if (goods == null) {
+            goods = goodsMapper.getGoodsById(id);
+            List<Product> goodsProduct = this.getProductByGoodsId(id);
             goods.setProductList(goodsProduct);
-            redisTemplate.opsForValue().set(key, goods,redisConfig.getRedisExpireTime(),TimeUnit.MINUTES);
-            //,redisConfig.getRedisExpireTime(),TimeUnit.MINUTES
+            redisTemplate.opsForValue().set(key, goods, redisConfig.getRedisExpireTime(), TimeUnit.MINUTES);
         }
         return goods;
     }
 
     /**
      * 获得所有商品
-     * @return
+     *
+     * @return x
      */
-    public List<Goods> getAllGoods(){return goodsMapper.getAllGoods();}
+    public List<Goods> getAllGoods() {
+        return goodsMapper.getAllGoods();
+    }
 
 }
