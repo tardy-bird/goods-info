12bddf659aab418343cba8f5ef32e9e2395ceb49
seekincs <seekincs@gmail.com>
2019-12-09 22:00:15 +0800

    fix RedisConfig typo.

--- src/main/java/com/tardybird/goodsinfo/config/RedisConfig.java
+++ src/main/java/com/tardybird/goodsinfo/config/RedisConfig.java
@@ -1,36 +1,28 @@
 package com.tardybird.goodsinfo.config;
 
-import io.swagger.models.auth.In;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
  * @author DIX
  * @version 1.0
- * @description
  * @date 2019/12/6 20:27
  */
 
 @Component
+@Getter
 public class RedisConfig {
-
     /**
      * redis缓存失效时间，单位分钟
      */
-    @Value("${GoodsInfo.redisExpireTime}")
+    @Value("${goods.redisExpireTime}")
     private Integer redisExpireTime;
 
     /**
      * 预提库存数量
      */
-    @Value("${GoodsInfo.preDeductQty}")
+    @Value("${goods.preDeductQuantity}")
     private Integer preDeductQty;
 
-    public Integer getRedisExpireTime() {
-        return redisExpireTime;
-    }
-
-    public Integer getPreDeductQty() {
-        return preDeductQty;
-    }
 }
