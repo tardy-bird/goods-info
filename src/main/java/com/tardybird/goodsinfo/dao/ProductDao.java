c138859abc1ac4e01a1b5502d8e8b62273197b69
seekincs <seekincs@gmail.com>
2019-12-24 09:46:32 +0800

    refactor dao codes.

--- src/main/java/com/tardybird/goodsinfo/dao/ProductDao.java
+++ src/main/java/com/tardybird/goodsinfo/dao/ProductDao.java
@@ -8,53 +8,74 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author nick
+ */
 @Repository
 public class ProductDao {
 
     final RedisTemplate<String, ProductPo> redisTemplateOfProducts;
-
     final ProductMapper productMapper;
-
     final RedisTemplate<String, List<String>> redisTemplateOfString;
 
-    public ProductDao(RedisTemplate<String, ProductPo> redisTemplateOfProducts, ProductMapper productMapper, RedisTemplate<String, List<String>> redisTemplateOfString) {
+    public ProductDao(RedisTemplate<String, ProductPo> redisTemplateOfProducts,
+                      ProductMapper productMapper,
+                      RedisTemplate<String, List<String>> redisTemplateOfString) {
         this.redisTemplateOfProducts = redisTemplateOfProducts;
         this.productMapper = productMapper;
         this.redisTemplateOfString = redisTemplateOfString;
     }
 
+    /**
+     * 扣库存
+     *
+     * @param productId 产品ID
+     * @param quantity  预扣数量
+     * @return true 扣成功
+     */
     public Boolean deductProductStock(Integer productId, Integer quantity) {
         String key = "Product_" + productId;
 
         ProductPo productPo = redisTemplateOfProducts.opsForValue().get(key);
 
+        // 从数据库中找
         if (productPo == null) {
             productPo = productMapper.getProductById(productId);
         }
 
+        // 如果产品不存在，则扣减失败
         if (productPo == null) {
             return false;
         }
 
         redisTemplateOfProducts.opsForValue().set(key, productPo);
 
+        // 预期扣减的数量大于实际数量
         Integer currentStock = productPo.getSafetyStock();
         if (currentStock < quantity) {
             return false;
         }
 
-
         productPo.setSafetyStock(currentStock - quantity);
 
+        // 更新Redis中关于产品的信息
         redisTemplateOfProducts.delete(key);
         redisTemplateOfProducts.opsForValue().set(key, productPo);
 
+        // 将更改过的产品ID存在一队列中，定时写回数据库
         collectChanges(String.valueOf(productPo.getId()));
 
         return true;
     }
 
+    /**
+     * 收集发生更改的产品ID
+     *
+     * @param productId 产品ID
+     */
     private void collectChanges(String productId) {
+
+        // Redis中的键
         String productChangeListKey = "Product_Changes";
         List<String> changeList = redisTemplateOfString.opsForValue().get(productChangeListKey);
 
