f9d93a7783cf2f72e0e6fd4c3c9c465dfd8e5426
seekincs <seekincs@gmail.com>
2019-12-24 09:51:05 +0800

    refactor domain codes.

--- src/main/java/com/tardybird/goodsinfo/domain/Brand.java
+++ src/main/java/com/tardybird/goodsinfo/domain/Brand.java
@@ -10,16 +10,15 @@ import lombok.ToString;
 import java.util.List;
 
 /**
- * @Author: 数据库与对象模型标准组
- * @Description:品牌对象
- * @Data:Created in 14:50 2019/12/11
+ * 品牌对象
+ *
+ * @author 数据库与对象模型标准组
+ * @date Created in 14:50 2019/12/11
  **/
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode(callSuper = true)
 public class Brand extends BrandPo {
-
     private List<GoodsPo> goodsPoList;
-
 }
